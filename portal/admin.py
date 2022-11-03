72922d0784a0df6de53ac5dc3f128ed904b9ba90
Yash Gupta <yashguptaab99@gmail.com>
2022-11-04 01:31:42 +0530

    feat: admin register

--- portal/admin.py
+++ portal/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer, Staffs
+from .models import Reservation, CustomUser, Admin, Staffs, Parking
+
+admin.site.register(Reservation);
+admin.site.register(CustomUser);
+admin.site.register(Admin);
+admin.site.register(Staffs);
+admin.site.register(Parking);
+
 
 
