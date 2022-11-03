316cdbbb1a72a9cd47a077afc9aae2594c97c644
Yash Gupta <yashguptaab99@gmail.com>
2022-11-04 02:19:28 +0530

    refactor: name

--- portal/admin.py
+++ portal/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Reservation, CustomUser, Admin, Staffs, Parking
+from .models import Customer, CustomUser, Admin, Staffs, Parking
 
-admin.site.register(Reservation);
+admin.site.register(Customer);
 admin.site.register(CustomUser);
 admin.site.register(Admin);
 admin.site.register(Staffs);
