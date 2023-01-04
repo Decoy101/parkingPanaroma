ed3b7205644dbf7a65ce5d743e22b261cddc54d1
Decoy101 <82807218+Decoy101@users.noreply.github.com>
2023-01-05 02:22:12 +0530

    refactor: Few changes to dashboard

--- portal/filters.py
+++ portal/filters.py
@@ -7,6 +7,8 @@ class DateInput(forms.DateInput):
     input_type = 'date'
 
 class ReservationsFilter(django_filters.FilterSet):
+    first_name = django_filters.CharFilter(lookup_expr='icontains')
+    last_name = django_filters.CharFilter(lookup_expr='icontains')
     check_in = django_filters.DateFilter(widget=DateInput(attrs={'type': 'date'}))
     check_out = django_filters.DateFilter(widget=DateInput(attrs={'type': 'date'}))
     class Meta:
